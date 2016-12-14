28d14e6ebd182996e68bf2d2d1937639eac7ce41
Vanlalhriata <vanlalhriata@gmail.com>
2016-12-14 13:46:36 +0530

    Installed laravel/socialite and configured for Facebook

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,7 +226,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
